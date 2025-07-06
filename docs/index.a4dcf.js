e93971baf425b4da03164125e1c0b7fae65dd367
Alexandr Bredikhin <akagami.beats@gmail.com>
2025-07-07 02:28:36 +0500

    build

--- docs/index.a4dcf.js
+++ docs/index.a4dcf.js
@@ -1,4 +1,4 @@
-System.register(["./application.js"], function (_export, _context) {
+System.register(["./application.9b67c.js"], function (_export, _context) {
   "use strict";
 
   var Application, canvas, $p, bcr, application;
